266bf5bf900edab1f0de87af34922e120baade87
Numan <numaniftikhar300@gmail.com>
2022-08-23 09:51:54 +0500

    Get value of input box

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const sayHello = () => {
-    setCount(count + 1);
+  const [data, setData] = useState(0)
+  const handleChange = (event) => {
+    setData(event.target.value);
   };
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={sayHello}>Count</button>
+      <h1 style={{height: '3rem'}}>{data}</h1>
+      <input onChange={handleChange} type="text"/>
     </div>
   );
 }
