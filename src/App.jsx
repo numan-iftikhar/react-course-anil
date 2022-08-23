da2fe87bb0b5d751a2fdead602b740c4c30e8bab
Numan <numaniftikhar300@gmail.com>
2022-08-23 10:25:49 +0500

    Get form data using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
+  const [name, setName] = useState("");
+  const [option, setOption] = useState("");
+  const [checked, setChecked] = useState(false);
+
+  console.log([name, option, checked]);
 
   return (
     <div className="App">
-      <h1 style={{ height: "3rem" }}>{toggle ? "hello" : null}</h1>
-      <button onClick={handleToggle}>Toggle</button>
+      <h2>Please Fill the Form Below</h2>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <br />
+      <br />
+      <select onChange={(e) => setOption(e.target.value)}>
+        <option value="">Select Option</option>
+        <option value="">hello</option>
+        <option value="">mellow</option>
+        <option value="">fellow</option>
+      </select>
+      <br />
+      <br />
+      <input type="checkbox" onChange={(e) => setChecked(e.target.checked)} />
+      <span>Terms and Conditions</span>
+      <br />
+      <br />
+      <button type="submit">Submit</button>
     </div>
   );
 }
